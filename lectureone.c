d3052f89fa8a86fe1a55c0d2527a8f14254b55a7
Lalith Sasubilli <lsasubilli@gmail.com>
2026-01-06 13:13:02 -0800

    example one

--- lectureone.c
+++ lectureone.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main(){
-	
+	char capital_A = 'A';
+	char sixty_five = 65;
+	printf("capital_a: %c %d\n", capital_A,capital_A);
+	printf("sixty_five: %c %d\n", sixty_five, sixty_five);	
 	return 0;
 }
 
